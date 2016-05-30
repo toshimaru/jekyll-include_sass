b34bad114936434943cd2f07ae77db2df26df3ae
toshimaru <me@toshimaru.net>
2016-05-30 13:00:50 +0900

    Initialize :gem: repo

---
+++ lib/jekyll/include_sass.rb
@@ -0,0 +1,7 @@
+require "jekyll/include_sass/version"
+
+module Jekyll
+  module IncludeSass
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'jekyll/include_sass'
+
+require 'minitest/autorun'
