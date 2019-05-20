59387cd5805e2d6c7096e1b58987e8c7afae3398
toshimaru <me@toshimaru.net>
2019-05-21 00:22:12 +0900

    include_sass {{ variable }} test

--- test/jekyll/include_sass_tag_test.rb
+++ test/jekyll/include_sass_tag_test.rb
@@ -2,6 +2,7 @@
 
 require 'test_helper'
 
+# https://github.com/jekyll/jekyll/blob/master/test/test_tags.rb
 class Jekyll::Tags::IncludeSassTagTest < JekyllUnitTest
   def test_include_sass
     content = <<~HTML
@@ -39,6 +40,25 @@ class Jekyll::Tags::IncludeSassTagTest < JekyllUnitTest
     assert_match "font: 100% Helvetica, sans-serif;", @result
   end
 
+  def test_read_variable
+    content = <<~HTML
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <style type="text/css">
+        {% include_sass {{ page.css }} %}
+        </style>
+      </head>
+      <body>
+        Your reading stylesheet is {{ page.css }}.
+      </body>
+      </html>
+    HTML
+  
+    create_post(content)
+    assert_match "font: 100% Helvetica, sans-serif;", @result
+  end
+  
   private
 
   def create_post(content)
@@ -47,7 +67,8 @@ class Jekyll::Tags::IncludeSassTagTest < JekyllUnitTest
     info = { filters: [Jekyll::Filters], registers: { site: site } }
     @converter = site.converters.find { |c| c.class == Jekyll::Converters::Markdown }
     payload = { "highlighter_prefix" => @converter.highlighter_prefix,
-                "highlighter_suffix" => @converter.highlighter_suffix }
+                "highlighter_suffix" => @converter.highlighter_suffix, 
+                "page" => { "css" => "scss_file.scss" } }
 
     @result = Liquid::Template.parse(content).render!(payload, info)
     @result = @converter.convert(@result)
