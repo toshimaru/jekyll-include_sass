9fecf5c7f853b5fc3f6588ab2f96e99397fdb762
Toshimaru <me@toshimaru.net>
2019-05-21 00:30:50 +0900

    Add test (#9)
    * rubocop -a
    
    * test refactoring
    
    * CONTENT -> HTML
    
    * include_sass {{ variable }} test
    
    * Refactoring

--- test/jekyll/include_sass_tag_test.rb
+++ test/jekyll/include_sass_tag_test.rb
@@ -2,58 +2,72 @@
 
 require 'test_helper'
 
+# https://github.com/jekyll/jekyll/blob/master/test/test_tags.rb
 class Jekyll::Tags::IncludeSassTagTest < JekyllUnitTest
-  def test_that_it_has_a_version_number
-    refute_nil ::Jekyll::IncludeSass::VERSION
-  end
-
-  def test_it_does_something_useful
-    refute_nil ::Jekyll::Tags::IncludeSassTag
-  end
-
   def test_include_sass
-    content = <<CONTENT
-<html>
-<head>
-  <meta charset="utf-8">
-  <style type="text/css">
-  {% include_sass sass_file.sass %}
-  </style>
-</head>
-<body>
-</body>
-</html>
-CONTENT
+    content = <<~HTML
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <style type="text/css">
+        {% include_sass sass_file.sass %}
+        </style>
+      </head>
+      <body>
+      </body>
+      </html>
+    HTML
 
-    create_post(content, { "read_posts" => true })
+    create_post(content)
     assert_match "font: 100% Helvetica, sans-serif;", @result
   end
 
   def test_include_scss
-    content = <<CONTENT
-<html>
-<head>
-  <meta charset="utf-8">
-  <style type="text/css">
-  {% include_sass scss_file.scss %}
-  </style>
-</head>
-<body>
-</body>
-</html>
-CONTENT
+    content = <<~HTML
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <style type="text/css">
+        {% include_sass scss_file.scss %}
+        </style>
+      </head>
+      <body>
+      </body>
+      </html>
+    HTML
 
-    create_post(content, { "read_posts" => true })
+    create_post(content)
     assert_match "font: 100% Helvetica, sans-serif;", @result
   end
 
-  def create_post(content, override = {}, converter_class = Jekyll::Converters::Markdown)
+  def test_read_variable
+    content = <<~HTML
+      <html>
+      <head>
+        <meta charset="utf-8">
+        <style type="text/css">
+        {% include_sass {{ page.css }} %}
+        </style>
+      </head>
+      <body>
+        Your reading stylesheet is {{ page.css }}.
+      </body>
+      </html>
+    HTML
+  
+    create_post(content, override: { "page" => { "css" => "scss_file.scss" } })
+    assert_match "font: 100% Helvetica, sans-serif;", @result
+  end
+  
+  private
+
+  def create_post(content, override: {})
     site = fixture_site
 
-    info = { :filters => [Jekyll::Filters], :registers => { :site => site } }
-    @converter = site.converters.find { |c| c.class == converter_class }
+    info = { filters: [Jekyll::Filters], registers: { site: site } }
+    @converter = site.converters.find { |c| c.class == Jekyll::Converters::Markdown }
     payload = { "highlighter_prefix" => @converter.highlighter_prefix,
-                "highlighter_suffix" => @converter.highlighter_suffix }
+                "highlighter_suffix" => @converter.highlighter_suffix }.merge(override)
 
     @result = Liquid::Template.parse(content).render!(payload, info)
     @result = @converter.convert(@result)
