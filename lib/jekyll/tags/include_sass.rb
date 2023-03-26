15aacf74fc87e05f0ea9013337c16e3325f180b0
toshimaru <me@toshimaru.net>
2023-03-26 10:08:03 +0900

    docs: Add docs for `IncludeSassTag` class

--- lib/jekyll/tags/include_sass.rb
+++ lib/jekyll/tags/include_sass.rb
@@ -2,6 +2,10 @@
 
 module Jekyll
   module Tags
+    # Render sass or scss files
+    #
+    # Usage: {% include_sass file.sass %}
+    #        {% include_scss file.scss %}
     class IncludeSassTag < IncludeTag
       include Jekyll::Filters
 
@@ -17,12 +21,12 @@ module Jekyll
         when '.scss'
           scssify(super)
         else
-          raise "Invalid file extension. Please specify sass or scss as a file."
+          raise 'Invalid file extension. Please specify sass or scss as a file.'
         end
       end
     end
   end
 end
 
-Liquid::Template.register_tag("include_sass", Jekyll::Tags::IncludeSassTag)
-Liquid::Template.register_tag("include_scss", Jekyll::Tags::IncludeSassTag)
+Liquid::Template.register_tag('include_sass', Jekyll::Tags::IncludeSassTag)
+Liquid::Template.register_tag('include_scss', Jekyll::Tags::IncludeSassTag)
